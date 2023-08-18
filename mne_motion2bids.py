7cda05c8a997542e69f78b8a9b222d608431314d
Julius Welzel <julius.welzel@gmail.com>
2023-08-18 14:04:20 +0200

    black linting

--- mne_motion2bids.py
+++ mne_motion2bids.py
@@ -5,15 +5,26 @@ from pathlib import Path
 from utls.motiondata import MotionData
 
 # specify BIDS root
-bids_root = Path(r'./data/bids')
+bids_root = Path(r"./data/bids")
 
 # load motion data to dataclass
-test_motion_data = MotionData.import_hasomed_imu(file=r'./data/exDataHasomed.csv')
+test_motion_data = MotionData.import_hasomed_imu(file=r"./data/exDataHasomed.csv")
 
 # transfer data to mne raw object
-info = mne.create_info(ch_names=test_motion_data.channels.name, sfreq=test_motion_data.info.SamplingFrequency, ch_types='bio')
+info = mne.create_info(
+    ch_names=test_motion_data.channels.name,
+    sfreq=test_motion_data.info.SamplingFrequency,
+    ch_types="bio",
+)
 raw = mne.io.RawArray(test_motion_data.time_series, info)
 
 # write to BIDS
-bids_path = BIDSPath(subject=test_motion_data.info.SubjectId, task=test_motion_data.info.TaskName, datatype='beh', root=bids_root)
-write_raw_bids(raw, bids_path, overwrite=True, allow_preload=True, format='EDF', verbose=True)
+bids_path = BIDSPath(
+    subject=test_motion_data.info.SubjectId,
+    task=test_motion_data.info.TaskName,
+    datatype="beh",
+    root=bids_root,
+)
+write_raw_bids(
+    raw, bids_path, overwrite=True, allow_preload=True, format="EDF", verbose=True
+)
--- utls/motiondata.py
+++ utls/motiondata.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 from utls.file_io import get_unit_from_type
 
+
 @dataclass
 class FileInfo:
     SubjectId: str
@@ -20,8 +21,20 @@ class FileInfo:
     TrackingSystemName: Optional[str] = None
 
 
-VALID_CHANNEL_TYPES = {"ACCEL", "ANGACCEL", "GYRO", "JNTANG", "LATENCY", "MAGN", "MISC", "ORNT", "POS", "VEL"}
+VALID_CHANNEL_TYPES = {
+    "ACCEL",
+    "ANGACCEL",
+    "GYRO",
+    "JNTANG",
+    "LATENCY",
+    "MAGN",
+    "MISC",
+    "ORNT",
+    "POS",
+    "VEL",
+}
 VALID_COMPONENT_TYPES = {"x", "y", "z", "quat_x", "quat_y", "quat_z", "quat_w", "n/a"}
+
 
 @dataclass
 class ChannelMetaData:
@@ -39,9 +52,14 @@ class ChannelMetaData:
 
     def __post_init__(self):
         if not all(ch in VALID_CHANNEL_TYPES for ch in self.ch_type):
-            raise ValueError(f"Invalid channel type {self.ch_type}. Must be one of {VALID_CHANNEL_TYPES}")
+            raise ValueError(
+                f"Invalid channel type {self.ch_type}. Must be one of {VALID_CHANNEL_TYPES}"
+            )
         if not all(comp in VALID_COMPONENT_TYPES for comp in self.component):
-            raise ValueError(f"Invalid component type {self.component}. Must be one of {VALID_COMPONENT_TYPES}")
+            raise ValueError(
+                f"Invalid component type {self.component}. Must be one of {VALID_COMPONENT_TYPES}"
+            )
+
 
 @dataclass
 class MotionData:
@@ -52,24 +70,28 @@ class MotionData:
 
     def __post_init__(self):
         if len(self.times) != self.time_series.shape[1]:
-            raise ValueError("The length of `times` should match the number of columns in `time_series`")
+            raise ValueError(
+                "The length of `times` should match the number of columns in `time_series`"
+            )
 
         if len(self.channels.name) != self.time_series.shape[0]:
-            raise ValueError("The number of `channel_names` should match the number of rows in `time_series`")
+            raise ValueError(
+                "The number of `channel_names` should match the number of rows in `time_series`"
+            )
 
     @classmethod
     def import_hasomed_imu(self, file: str):
         """
-        This function reads a file and returns a MotionData object. 
+        This function reads a file and returns a MotionData object.
 
         Parameters:
         file (str): path to the .csv file
 
         Returns:
-        MotionData: an object of class MotionData that includes FileInfo 
+        MotionData: an object of class MotionData that includes FileInfo
         object with metadata from the file,
-        a 1D numpy array with time values, 
-        a list of channel names, 
+        a 1D numpy array with time values,
+        a list of channel names,
         and a 2D numpy array with the time series data.
 
         The file structure is assumed to be as follows:
@@ -83,23 +105,25 @@ class MotionData:
         Also, error checking and exception handling has been kept minimal for simplicity.
         You might want to add more robust error handling for a production-level application.
         """
-        with open(file, 'r') as f:
+        with open(file, "r") as f:
             lines = f.readlines()
 
         # Keep track of where the metadata ends and the time series data begins
         data_start_idx = 0
 
         # Instantiate empty FileInfo
-        info = FileInfo(SubjectId="", TaskName="", SamplingFrequency=100.0)  # default SamplingFrequency to 100.0 if not found
+        info = FileInfo(
+            SubjectId="", TaskName="", SamplingFrequency=100.0
+        )  # default SamplingFrequency to 100.0 if not found
 
         for idx, line in enumerate(lines):
             # Metadata ends when we encounter a line that doesn't start with '#'
-            if not line.startswith('#'):
+            if not line.startswith("#"):
                 data_start_idx = idx
                 break
 
             # Extract fields for FileInfo
-            parts = line.strip().split(';')
+            parts = line.strip().split(";")
 
             if "Patient-ID" in line:
                 info.SubjectId = parts[2]
@@ -111,7 +135,7 @@ class MotionData:
             # Add more fields as necessary here...
 
         # Create DataFrame from the time series data
-        data = pd.read_csv(file, skiprows=data_start_idx - 1, delimiter=';')
+        data = pd.read_csv(file, skiprows=data_start_idx - 1, delimiter=";")
 
         # Extract the channel names from the column names of the DataFrame
         channel_names = data.columns.tolist()
@@ -119,35 +143,46 @@ class MotionData:
         # Convert time to numpy array
         times = np.linspace(0, data.shape[0] / info.SamplingFrequency, data.shape[0])
 
-        type_imu = ['Acc', 'Gyro', 'Mag'] 
+        type_imu = ["Acc", "Gyro", "Mag"]
         # drop non relevant columns
-        filtered_col_names = [col for col in channel_names if not any(sensor in col for sensor in type_imu)]
-        channel_names = [col for col in channel_names if any(sensor in col for sensor in type_imu)]
+        filtered_col_names = [
+            col
+            for col in channel_names
+            if not any(sensor in col for sensor in type_imu)
+        ]
+        channel_names = [
+            col for col in channel_names if any(sensor in col for sensor in type_imu)
+        ]
         # have consited numbering in channel_names and replace . with _
-        channel_names = [col.replace('.', '_') for col in channel_names]
+        channel_names = [col.replace(".", "_") for col in channel_names]
         # check if channel_name is numbered, if not append 0
         for idx, name in enumerate(channel_names):
             if not name[-1].isdigit():
-                channel_names[idx] = name + '_0'
+                channel_names[idx] = name + "_0"
 
         # define ChannelMetaData
         # extract channel component
-        channel_components = [name.split('_')[-2][-1].lower() for name in channel_names]
+        channel_components = [name.split("_")[-2][-1].lower() for name in channel_names]
         # extract channel type
-        channel_types = [name.split('_')[0][:-1] for name in channel_names]
+        channel_types = [name.split("_")[0][:-1] for name in channel_names]
         # convert to meaningful types (Acc -> ACCEL, Gyro -> GYRO, Mag -> MAGN)
-        channel_types = ['ACCEL' if 'Acc' in type else 'GYRO' if 'Gyro' in type else 'MAGN' for type in channel_types]
+        channel_types = [
+            "ACCEL" if "Acc" in type else "GYRO" if "Gyro" in type else "MAGN"
+            for type in channel_types
+        ]
         # get channel units
         channel_units = get_unit_from_type(channel_types)
         # get tracked point
-        channels_tracked_point = [name.split('_')[0][-1] for name in channel_names]
+        channels_tracked_point = [name.split("_")[0][-1] for name in channel_names]
 
         # update ChannelMetaData
-        channels = ChannelMetaData(name=channel_names,
-                                        component=channel_components,
-                                        ch_type=channel_types,
-                                        tracked_point=channels_tracked_point,
-                                        units=channel_units)
+        channels = ChannelMetaData(
+            name=channel_names,
+            component=channel_components,
+            ch_type=channel_types,
+            tracked_point=channels_tracked_point,
+            units=channel_units,
+        )
 
         time_series = data.drop(columns=filtered_col_names).to_numpy().T  # transpose
 
